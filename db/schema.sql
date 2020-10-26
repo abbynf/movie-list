87afb6d509dc3d426a30e1efb5caec3c839a2eb9
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-10-25 21:47:45 -0600

    created database and table

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,9 @@
--- Create database
+CREATE DATABASE movies_db;
+
+USE movies_db;
+CREATE TABLE movies(
+    id INT NOT NULL AUTO_INCREMENT,
+    movie_name VARCHAR(120),
+    watched BOOLEAN,
+    PRIMARY KEY (id)
+);
