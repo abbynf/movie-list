7adff7d4fbc4ef6947ff4fbc9d1e622c349b182d
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-10-25 21:56:25 -0600

    intialized mysql server connection

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,18 @@
 // Set up mySQL connection
+const mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "@Ecuador14",
+    database: "movies_db"
+})
+
+connection.connect(function(err){
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+})
+
 
 // Export connection
