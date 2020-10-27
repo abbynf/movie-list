9187985196e63bee1c64d25f18275c367d30acd3
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-10-26 21:49:54 -0600

    created functions to interact with orm

--- models/movies.js
+++ models/movies.js
@@ -1,1 +1,23 @@
 // Import ORM to create functions that will interact with database
+const orm = require('./../config/orm.js')
+
+var movie = {
+    all: function(){
+        // Call orm to view all movies
+        orm.selectAll();
+    },
+    create: function(movieTitle){
+        // call orm to insert the chosen movie title
+        orm.insertOne(movieTitle, function(res){
+            console.log(res)
+        })
+    },
+    update: function(movieTitle, id, seen){
+        // Call orm to update chosen movie to watched or not watched
+        orm.updateOne(movieTitle, id, seen, function(res){
+            console.log(res)
+        })
+    }
+}
+
+module.exports = movie;
