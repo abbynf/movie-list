fb9de1847b70c770ad565ed250fe1d2fff13a58e
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-10-26 21:11:52 -0600

    added function logic for select all

--- config/orm.js
+++ config/orm.js
@@ -7,7 +7,10 @@ var connection = require("./connection.js");
 // Create custom orm
 var orm = {
     selectAll: function(){
-
+        connection.query("SELECT * FROM movies", function(err, res){
+            if (err) throw err;
+            console.table(res);
+        })
     },
     insertOne: function(){
 
