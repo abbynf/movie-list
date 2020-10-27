431da70f142953ce36c404d4d51fae1b5da3bbb4
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-10-26 21:17:11 -0600

    added function for inserting new title

--- config/orm.js
+++ config/orm.js
@@ -8,12 +8,18 @@ var connection = require("./connection.js");
 var orm = {
     selectAll: function(){
         connection.query("SELECT * FROM movies", function(err, res){
+            // display all movies in the movies table
             if (err) throw err;
             console.table(res);
         })
     },
-    insertOne: function(){
-
+    insertOne: function(movieTitle){
+        // insert dynamic movie title into database
+        connection.query("INSERT INTO movies (movie_name, watched) VALUES (?, false);", [movieTitle], function(err, res){
+            if (err) throw err;
+            // tell user it was successful
+            console.log("Successfully added " + movieTitle)
+        })
     },
     updateOne: function(){
 
