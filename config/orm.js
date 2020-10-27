e38c8f862056cadafb8284fffd0a2670a1db9a59
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-10-26 21:07:28 -0600

    added basic orm function names and exported orm

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,22 @@
 // Create ORM
+
+// Import mysql connection from connection.js
+var connection = require("./connection.js");
+
+
+// Create custom orm
+var orm = {
+    selectAll: function(){
+
+    },
+    insertOne: function(){
+
+    },
+    updateOne: function(){
+
+    }
+}
+
+// export orm
+module.exports = orm;
+
