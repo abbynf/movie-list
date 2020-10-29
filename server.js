a78178d32211d01a59dec8ee189015dad3178cf7
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-10-28 19:11:24 -0600

    added middleware

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const app = express();
 
 var PORT = 8080;
 
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var routes = require("./controllers/moviesController")
+
+app.use(routes);
+
 app.listen(PORT, function(){
     console.log("Server listening on PORT " + PORT);
 })
+
